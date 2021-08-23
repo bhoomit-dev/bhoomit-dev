b59147f97920f4aee52587c500605e86d5325163
bhoomit-dev <88977011+bhoomit-dev@users.noreply.github.com>
2021-08-23 23:23:53 +0530

    added readme

---
+++ README.m
@@ -0,0 +1,43 @@
+<h1 align="center">Hi 👋, I'm Bhoomit Ganatra</h1>
+<h3 align="center">A passionate web developer from India</h3>
+
+<div align="left" inline>
+<span align="left"> <img src="https://komarev.com/ghpvc/?username=bhoomit-dev&label=Profile%20views&color=0e75b6&style=flat" alt="Bhoomit" /> </span>
+<span align="left"> <a href="https://twitter.com/bhoomit-dev" target="blank"><img src="https://img.shields.io/twitter/follow/bhoomit-dev?logo=twitter&style=for-the-badge" alt="Bhoomit" /></a> </span>
+</div>
+
+### My GitHub Stats
+
+<div>
+<img align="center" width="49%" src="https://github-readme-streak-stats.herokuapp.com/?user=bhoomit-dev&" alt="bhoomit-dev" />
+<img align="center" width="49%"  src="https://github-readme-stats.vercel.app/api?username=bhoomit-dev&show_icons=true&locale=en" alt="bhoomit-dev" />
+<br /><br />
+<img src="https://activity-graph.herokuapp.com/graph?username=bhoomit-dev&theme=xcode" alt="bhoomit-dev" />
+</div>
+
+### Languages and Tools:
+
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+ <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> 
+</a>
+<a href="https://nodejs.org" target="_blank">
+ <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-plain-wordmark.svg" alt="nodejs" width="40" height="40"/> 
+</a>
+<a href="https://www.mysql.com/" target="_blank"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> 
+</a>
+<a href="https://www.mongodb.com/" target="_blank"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> 
+</a>
+<a href="https://reactjs.org/" target="_blank"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> 
+</a> 
+<a href="https://vuejs.org/" target="_blank"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" width="40" height="40"/> 
+</a>
+<a href="https://nuxtjs.org/" target="_blank"> 
+    <img src="https://www.vectorlogo.zone/logos/nuxtjs/nuxtjs-icon.svg" alt="nuxtjs" width="40" height="40"/> 
+</a>
+<a href="https://getbootstrap.com" target="_blank"> 
+    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> 
+</a>
